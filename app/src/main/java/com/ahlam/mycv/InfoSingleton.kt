7e029e2bab4785f2eee1fba0a762cdbf4307ac78
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-04-06 11:49:19 +0300

    remove some files

--- app/src/main/java/com/ahlam/mycv/InfoSingleton.kt
+++ app/src/main/java/com/ahlam/mycv/InfoSingleton.kt
@@ -11,10 +11,15 @@
 
 package com.ahlam.mycv
 
+import com.ahlam.mycv.model.EntityContactInfo
+import com.ahlam.mycv.model.EntityEdu
+import com.ahlam.mycv.model.EntityPersonInfo
 import com.ahlam.mycv.utilities.FileManager
 import org.json.JSONObject
 
-
+/**
+ * this class acts as viewModel role. It prepares the info to be displayed in the views.
+ */
 
 class InfoSingleton {
 
@@ -23,9 +28,31 @@ class InfoSingleton {
         //get json file data
         val text: String = FileManager.getInfoFileData()
 
-        //create json
+        //form json data
         val jsonAll: JSONObject = JSONObject(text)
 
-        //todo create all data
+
+        //objects
+        val personal_info =  JsonParser.ofObject<EntityPersonInfo>(getStr(R.string.personal_info))
+        val contact_info = JsonParser.ofObject<EntityContactInfo>(getStr(R.string.contact_info))
+
+        //arrays
+        val work_experience = JsonParser.ofArray<EntityJob>(getStr(R.string.work_experience))
+        val education = JsonParser.ofArray<EntityEdu>(getStr(R.string.education))
+
+
+        val professional_skills = JsonParser.ofArray<String>(getStr(R.string.professional_skills))
+        val languages = JsonParser.ofArray<String>(getStr(R.string.languages_I_speak))
+        val techniques = JsonParser.ofArray<String>(getStr(R.string.techniques))
+        val design_patterns = JsonParser.ofArray<String>(getStr(R.string.design_patterns))
+        val tech_tools = JsonParser.ofArray<String>(getStr(R.string.tech_tools))
+        val supporting_tools = JsonParser.ofArray<String>(getStr(R.string.supporting_tools))
+        val IT_courses = JsonParser.ofArray<String>(getStr(R.string.IT_courses))
+        val other_courses = JsonParser.ofArray<String>(getStr(R.string.other_courses))
+        val personal_skills = JsonParser.ofArray<String>(getStr(R.string.personal_skills))
+        val familiar_with_OSs = JsonParser.ofArray<String>(getStr(R.string.familiar_with_OSs))
+        val methodologies = JsonParser.ofArray<String>(getStr(R.string.methodologies))
+        val hobbies = JsonParser.ofArray<String>(getStr(R.string.hobbies))
+        val other_skills = JsonParser.ofArray<String>(getStr(R.string.other_skills))
     }
 }
