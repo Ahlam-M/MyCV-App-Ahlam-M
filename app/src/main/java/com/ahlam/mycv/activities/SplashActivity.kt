28b8f813bec05d30ea536d1437c012bda87feb1f
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-04-10 17:47:56 +0300

    fix splash svg images

--- app/src/main/java/com/ahlam/mycv/activities/SplashActivity.kt
+++ app/src/main/java/com/ahlam/mycv/activities/SplashActivity.kt
@@ -11,19 +11,33 @@
 
 package com.ahlam.mycv.activities
 
+import android.content.Intent
+import android.graphics.Picture
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.ahlam.mycv.*
-import com.ahlam.mycv.model.EntityContactInfo
-import com.ahlam.mycv.model.EntityPersonInfo
+import java.util.*
+import kotlin.concurrent.schedule
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        findViewById<TextView>(R.id.txt_name).text = InfoSingleton.personal_info.name
+        findViewById<TextView>(R.id.txt_profession_title).text = InfoSingleton.personal_info.profession_title
 
+
+        Timer().schedule(33000){
+
+            val intent = Intent(this@SplashActivity, IntroActivity::class.java)
+            startActivity(intent)
+            this@SplashActivity.finish()
+        }
 
     }
 }
