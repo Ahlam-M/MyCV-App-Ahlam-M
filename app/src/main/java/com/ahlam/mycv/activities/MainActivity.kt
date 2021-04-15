395aaebd57c9512d257bc571a0f699b945814bd2
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-04-15 13:02:31 +0300

    custom actionbar utility

--- app/src/main/java/com/ahlam/mycv/activities/MainActivity.kt
+++ app/src/main/java/com/ahlam/mycv/activities/MainActivity.kt
@@ -2,11 +2,16 @@ package com.ahlam.mycv.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahlam.mycv.InfoSingleton
 import com.ahlam.mycv.R
+import com.ahlam.mycv.utilities.setCustomActionbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //show custom actionbar
+        setCustomActionbar(this@MainActivity, InfoSingleton.personal_info.name + " CV")
     }
 }
---
+++ app/src/main/java/com/ahlam/mycv/utilities/ActionbarCustom.kt
@@ -0,0 +1,37 @@
+/*
+ * ActionbarCustom.kt
+ *
+ * Created on 15 Apr 2021
+ * for "MyCV-Ahlam M"
+ * Copyright (c) 2021. All rights reserved.
+ *
+ * by developer Ahlam Mhmd
+ * Github : Ahlam-M
+ */
+
+package com.ahlam.mycv.utilities
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.ahlam.mycv.R
+
+
+/* this function is used to show custom actionbar in @activity */
+fun setCustomActionbar(activity: AppCompatActivity, title: String){
+    //allow custom view
+    activity.supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+    //get view
+    var tv : TextView = activity.layoutInflater.inflate(R.layout.actionbar_text, null) as TextView
+    //set text
+    tv.text = title
+    //set params
+    tv.layoutParams = ActionBar.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.MATCH_PARENT
+    )
+    //display
+    activity.supportActionBar?.customView = tv
+}
