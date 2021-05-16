5c514dcfe28e5cd52f07eaf475f24c4d258e25c3
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-05-16 19:10:33 +0300

    initiation

---
+++ app/src/main/java/com/ahlam/mycv/fragments/PersonalInfoFragment.kt
@@ -0,0 +1,43 @@
+/*
+ * PersonalInfoFragment.kt
+ *
+ * Created on 16 May 2021
+ * for "MyCV-Ahlam M"
+ * Copyright (c) 2021. All rights reserved.
+ *
+ * by developer Ahlam Mhmd
+ * Github : Ahlam-M
+ */
+
+package com.ahlam.mycv.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ahlam.mycv.R
+import com.ahlam.mycv.utilities.typeface
+
+
+class PersonalInfoFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PersonalInfoFragment()
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_personal_info, container, false)
+
+        typeface(rootView)
+        return rootView
+    }
+
+
+}
