d6bd0915e81469611d821d273e5267e0c1fb05f4
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-05-20 19:24:45 +0300

    personal skills shaps done

--- app/src/main/java/com/ahlam/mycv/fragments/PersonalInfoFragment.kt
+++ app/src/main/java/com/ahlam/mycv/fragments/PersonalInfoFragment.kt
@@ -16,6 +16,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.ahlam.mycv.InfoSingleton
 import com.ahlam.mycv.R
 import com.ahlam.mycv.utilities.typeface
 
@@ -34,6 +37,19 @@ class PersonalInfoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_personal_info, container, false)
+
+
+        val tvs : Array<TextView> = arrayOf(
+            rootView.findViewById<TextView>(R.id.txt_discipline),
+        rootView.findViewById<TextView>(R.id.txt_teamwork),
+        rootView.findViewById<TextView>(R.id.txt_hardwork),
+        rootView.findViewById<TextView>(R.id.txt_lead),
+        rootView.findViewById<TextView>(R.id.txt_learn),
+        rootView.findViewById<TextView>(R.id.txt_motivate))
+
+        for(i in tvs.indices){
+            tvs[i].text = InfoSingleton.personal_skills[i]
+        }
 
         typeface(rootView)
         return rootView
