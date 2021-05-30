baf82aefbc333b11e72a66d1d5070e81cf3e83ab
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-05-30 14:53:42 +0300

    contact info actions intents done

--- app/src/main/java/com/ahlam/mycv/activities/ContactMeActivity.kt
+++ app/src/main/java/com/ahlam/mycv/activities/ContactMeActivity.kt
@@ -11,16 +11,21 @@
 
 package com.ahlam.mycv.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.ahlam.mycv.APIs
 import com.ahlam.mycv.InfoSingleton
 import com.ahlam.mycv.R
 import com.ahlam.mycv.getStr
 import com.ahlam.mycv.utilities.setCustomActionbar
 import com.ahlam.mycv.utilities.typeface
+
 
 class ContactMeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,14 +38,16 @@ class ContactMeActivity : AppCompatActivity() {
             getStr(R.string.profession_title),
             getStr(R.string.dob),
             getStr(R.string.social_state),
-            getStr(R.string.languages))
+            getStr(R.string.languages)
+        )
 
         val lblPersonalInfoval : Array<String> = arrayOf(
             InfoSingleton.personal_info.name,
             InfoSingleton.personal_info.profession_title,
             InfoSingleton.personal_info.dob,
             InfoSingleton.personal_info.social_state,
-            InfoSingleton.personal_info.languages)
+            InfoSingleton.personal_info.languages
+        )
 
         for(i in lblPersonalInfo.indices){
             val v : View = View.inflate(this, R.layout.item_personal_info, null)
@@ -59,24 +66,36 @@ class ContactMeActivity : AppCompatActivity() {
             getStr(R.string.email),
             getStr(R.string.whatsapp),
             getStr(R.string.linkedin),
-            getStr(R.string.github))
+            getStr(R.string.github)
+        )
 
         val lblContactInfoval : Array<String> = arrayOf(
             InfoSingleton.contact_info.phone,
             InfoSingleton.contact_info.email,
             InfoSingleton.contact_info.whatsapp,
             InfoSingleton.contact_info.linkedin,
-            InfoSingleton.contact_info.github)
+            InfoSingleton.contact_info.github
+        )
+
+        val lblContactAction : Array<Intent> = arrayOf(
+            Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + InfoSingleton.contact_info.phone)),
+            Intent.createChooser(Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + InfoSingleton.contact_info.email)), getStr(R.string.email)),
+            Intent.createChooser(Intent(Intent.ACTION_VIEW, Uri.parse(APIs.whatsapp + InfoSingleton.contact_info.whatsapp)), getStr(R.string.whatsapp)),
+            Intent(Intent.ACTION_VIEW, Uri.parse(APIs.linkedin + InfoSingleton.contact_info.linkedin)),
+            Intent(Intent.ACTION_VIEW, Uri.parse(APIs.github + InfoSingleton.contact_info.github)),
+        )
 
         for(i in lblContactInfo.indices){
-            val v : View = View.inflate(this, R.layout.item_personal_info, null)
+            val v : View = View.inflate(this, R.layout.item_contact_info, null)
             //fill in data
             v.findViewById<TextView>(R.id.txt_label).text = lblContactInfo[i] + " : "
             v.findViewById<TextView>(R.id.txt_info).text = lblContactInfoval[i]
+            v.findViewById<Button>(R.id.btn_go).setOnClickListener { startActivity(lblContactAction[i]) }
 
             //add it to contact section view
             findViewById<LinearLayout>(R.id.ll_contact_info).addView(v)
         }
+
 
         //set custom font (after adding all views)
         typeface(this@ContactMeActivity)
